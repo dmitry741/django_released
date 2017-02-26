e7b2cb04bfe5095017701bf81b72299406b23300
dmitry741 <dmitrypavlov74@gmail.com>
2017-02-26 19:02:02 +0300

    templetes for work and education
    home

--- first_app_django/mainapp/views.py
+++ first_app_django/mainapp/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, render_to_response
 
 def main(request):
-    my_name = 'Pavlov Dmitry'
-    return render_to_response('index.html', {'name': my_name})
+    my_dict1 = {'name': 'Павлов'}
+    return render_to_response('index.html', my_dict1)
