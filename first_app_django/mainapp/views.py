0664cff30da157595b92cc908f8f9996cc1633f2
dmitry741 <dmitrypavlov74@gmail.com>
2017-02-26 20:15:18 +0300

    add second parameter
    home

--- first_app_django/mainapp/views.py
+++ first_app_django/mainapp/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, render_to_response
 
 def main(request):
-    my_dict1 = {'name': 'Павлов'}
+    my_dict1 = {'name': 'Павлов Дмитрий', 'project': 'AboutMe'}
     return render_to_response('index.html', my_dict1)
