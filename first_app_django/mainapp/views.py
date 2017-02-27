4e2aedfc7736e589aad30c19272d86842cb63b83
DMITRY PAVLOV <dmitrypavlov74@gmail.com>
2017-02-27 10:49:18 +0300

    templates for education and work
    ridan

--- first_app_django/mainapp/views.py
+++ first_app_django/mainapp/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render, render_to_response
 
 def main(request):
-    my_dict1 = {'name': 'Павлов Дмитрий', 'project': 'AboutMe'}
-    return render_to_response('index.html', my_dict1)
+    edu_list = ['Нижегородский универстет им. Лобачевского. Механико-математический факультет.',
+                'Online Geekbrains. Специальность Python разработчик.']
+    work_list = ['ЗАО НКТ. Разработчик С++.', 'АО Ридан. Разработчик C#.']
+    caption = 'Павлов Дмитрий. AboutMe project.'
+
+    my_dict = {'edu_list': edu_list,
+               'work_list': work_list,
+               'caption': caption}
+
+    return render_to_response('index.html', my_dict)
