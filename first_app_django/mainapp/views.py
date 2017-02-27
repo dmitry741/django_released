35ec7498b95885a3cfeb02fdc580ddb4f14b24f9
DMITRY PAVLOV <dmitrypavlov74@gmail.com>
2017-02-27 12:02:55 +0300

    add first name and second name separetly
    ridan

--- first_app_django/mainapp/views.py
+++ first_app_django/mainapp/views.py
@@ -4,10 +4,14 @@ def main(request):
     edu_list = ['Нижегородский универстет им. Лобачевского. Механико-математический факультет.',
                 'Online Geekbrains. Специальность Python разработчик.']
     work_list = ['ЗАО НКТ. Разработчик С++.', 'АО Ридан. Разработчик C#.']
-    caption = 'Павлов Дмитрий. AboutMe project.'
+    first_name = 'павлов'
+    second_name = 'дмитрий'
+    project_name = 'AboutMe'
 
     my_dict = {'edu_list': edu_list,
                'work_list': work_list,
-               'caption': caption }
+               'first_name': first_name,
+               'second_name': second_name,
+               'project_name': project_name}
 
     return render_to_response('index.html', my_dict)
