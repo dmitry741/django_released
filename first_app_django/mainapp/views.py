4b483ea0ecace29a89cfb993658ece09f640ac6f
DMITRY PAVLOV <dmitrypavlov74@gmail.com>
2017-03-15 14:47:03 +0300

    continue to making main menu
    ridan

--- first_app_django/mainapp/views.py
+++ first_app_django/mainapp/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render, render_to_response
 
 
+def get_mainmenu_captions():
+    return ['Главная', 'Резюме', 'Мои проекты', 'Увлечения', 'Контакты']
+
+
+def get_mainmenu_links():
+    return ['/', '/cv', '/projects', '/hobbies', '/contacts']
+
+
 class MyMainMenuItem:
     def __init__(self, menuItem, link, active):
         self.caption = menuItem
@@ -14,11 +22,13 @@ class MyMainMenuItem:
 class MyMainMenuManager:
     def __init__(self, index):
         self.menuItemList = []
-        self.menuItemList.append(MyMainMenuItem('Главная', '/', index == len(self.menuItemList)))
-        self.menuItemList.append(MyMainMenuItem('Резюме', '/cv', index == len(self.menuItemList)))
-        self.menuItemList.append(MyMainMenuItem('Мои проекты', '/projects', index == len(self.menuItemList)))
-        self.menuItemList.append(MyMainMenuItem('Увлечения', '/hobbies', index == len(self.menuItemList)))
-        self.menuItemList.append(MyMainMenuItem('Контакты', '/contacts', index == len(self.menuItemList)))
+
+        list_caption = get_mainmenu_captions()
+        list_links = get_mainmenu_links()
+        count = len(list_caption)
+
+        for i in range(count):
+            self.menuItemList.append(MyMainMenuItem(list_caption[i], list_links[i], index == len(self.menuItemList)))
 
 
 def get_common_menu_list(index):
@@ -26,51 +36,34 @@ def get_common_menu_list(index):
     return menu_manager.menuItemList
 
 
-def main(request):
+def get_dict(index):
     page_title = 'AboutMe project'
-    menu_list = get_common_menu_list(0)
+    menu_list = get_common_menu_list(index)
+    small_caption = get_mainmenu_captions()
+    small_caption[0] = None
 
     my_dict = {'page_title': page_title,
-               'menu_list': menu_list}
+               'menu_list': menu_list,
+               'small_caption': small_caption[index]}
 
-    return render_to_response('index.html', my_dict)
+    return my_dict
+
+
+def main(request):
+    return render_to_response('index.html', get_dict(0))
 
 
 def cv(request):
-    page_title = 'AboutMe project'
-    menu_list = get_common_menu_list(1)
-
-    my_dict = {'page_title': page_title,
-               'menu_list': menu_list}
-
-    return render_to_response('cv.html', my_dict)
+    return render_to_response('cv.html', get_dict(1))
 
 
 def projects(request):
-    page_title = 'AboutMe project'
-    menu_list = get_common_menu_list(2)
-
-    my_dict = {'page_title': page_title,
-               'menu_list': menu_list}
-
-    return render_to_response('projects.html', my_dict)
+    return render_to_response('projects.html', get_dict(2))
 
 
 def hobbies(request):
-    page_title = 'AboutMe project'
-    menu_list = get_common_menu_list(3)
-
-    my_dict = {'page_title': page_title,
-               'menu_list': menu_list}
-
-    return render_to_response('hobbies.html', my_dict)
+    return render_to_response('hobbies.html', get_dict(3))
 
 
 def contacts(request):
-    page_title = 'AboutMe project'
-    menu_list = get_common_menu_list(4)
-
-    my_dict = {'page_title': page_title,
-               'menu_list': menu_list}
-
-    return render_to_response('contacts.html', my_dict)
+    return render_to_response('contacts.html', get_dict(4))
