31baeafc2e65150c610719d891e363b2e3349f20
DMITRY PAVLOV <dmitrypavlov74@gmail.com>
2017-05-31 11:33:38 +0300

    Revert "change django form. apply style"
    This reverts commit f3891ae2db126bf0846946b9907afaf416d97b7c.

--- first_app_django/mainapp/djform.py
+++ first_app_django/mainapp/djform.py
@@ -3,7 +3,7 @@ from django import forms
 
 
 class MyContactForm(forms.Form):
-    # sender = forms.EmailField(label='Ваш email')
-    sender = forms.EmailField(label='Ваш email', widget=forms.TextInput(attrs={'class' : 'form-control', 'placeholder' : 'Ваш Email'}))
-    subject = forms.CharField(label='Тема', max_length=100, widget=forms.TextInput(attrs={'class' : 'form-control', 'placeholder' : 'Тема'}))
-    message = forms.CharField(label='Сообщение', widget=forms.Textarea(attrs={'class' : 'form-control', 'placeholder' : 'Сообщение'}))
+
+    sender = forms.EmailField(label='Ваш email')
+    subject = forms.CharField(label='Тема', max_length=100)
+    message = forms.CharField(label='Сообщение', widget=forms.Textarea)
