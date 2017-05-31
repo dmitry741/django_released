a0f2675460e5b3b237737802b4f652cc5b559e90
DMITRY PAVLOV <dmitrypavlov74@gmail.com>
2017-05-31 17:17:12 +0300

    project page is changed
    ridan

--- first_app_django/mainapp/djform.py
+++ first_app_django/mainapp/djform.py
@@ -7,3 +7,9 @@ class MyContactForm(forms.Form):
     sender = forms.EmailField(label='Ваш email')
     subject = forms.CharField(label='Тема', max_length=100)
     message = forms.CharField(label='Сообщение', widget=forms.Textarea)
+
+
+    # user = forms.CharField(label='Имя', max_length=50, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    # sender = forms.EmailField(label='Ваш email', widget=forms.TextInput(attrs={'class' : 'form-control'}))
+    # subject = forms.CharField(label='Тема', max_length=100, widget=forms.TextInput(attrs={'class' : 'form-control'}))
+    # message = forms.CharField(label='Сообщение', widget=forms.Textarea(attrs={'class' : 'form-control'}))
